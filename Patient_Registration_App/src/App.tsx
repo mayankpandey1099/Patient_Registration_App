c47d56a8e2b0fd898932eb81c2fcb772e6fab2b0
mayank pandey <mayankpandey1099@gmail.com>
2025-05-23 09:25:12 +0530

    added protected routing

--- Patient_Registration_App/src/App.tsx
+++ Patient_Registration_App/src/App.tsx
@@ -1,8 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import LoginRegister from "./components/LoginRegister";
 import PatientForm from "./components/PatientForm";
 import PatientList from "./components/PatientList";
 import QueryInterface from "./components/QueryInterface";
+import { verifyToken } from "./auth";
+
+const ProtectedLayout: React.FC = () => {
+  const token = localStorage.getItem("token");
+  const isAuthenticated = verifyToken(token);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
 
 const App: React.FC = () => {
   return (
@@ -10,9 +24,12 @@ const App: React.FC = () => {
       <div className="bg-gray-100 min-h-screen">
         <Routes>
           <Route path="/" element={<LoginRegister />} />
-          <Route path="/register-patient" element={<PatientForm />} />
-          <Route path="/patient-list" element={<PatientList />} />
-          <Route path="/query" element={<QueryInterface />} />
+
+          <Route element={<ProtectedLayout />}>
+            <Route path="/register-patient" element={<PatientForm />} />
+            <Route path="/patient-list" element={<PatientList />} />
+            <Route path="/query" element={<QueryInterface />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
